e1d7bc8aba107161ba25d6d7312889dd50e6a522
mazy99 <mazy.1999@mail.ru>
2025-12-07 17:26:42 +0300

    delete main in file

--- tasks/calculator.py
+++ tasks/calculator.py
@@ -47,14 +47,3 @@ class Calculator:
 
     def __str__(self) -> str:
         return f"Результат сложения: {self.calculate()}"
-
-
-if __name__ == "__main__":
-    a_input = InputReader.read("Введите первое значение: ")
-    b_input = InputReader.read("Введите второе значение: ")
-
-    a_parsed = ValueParser.parse_int(a_input)
-    b_parsed = ValueParser.parse_int(b_input)
-
-    calculator = Calculator(a_parsed, b_parsed)
-    print(calculator)
