e8f6208d2f8e1e7105f1167dff781ae47dd66afe
mazy99 <mazy.1999@mail.ru>
2025-12-07 17:59:43 +0300

    add matrix file

---
+++ tasks/matrix_generate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import random
+from typing import List
+
+
+class MatrixGenerator:
+    def __init__(self, rows: int, cols: int, min_value: int, max_value: int) -> None:
+        self.rows = rows
+        self.cols = cols
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def generate_matrix(self) -> List[List[int]]:
+        matrix = []
+        for _ in range(self.rows):
+            row = [
+                random.randint(self.min_value, self.max_value) for _ in range(self.cols)
+            ]
+            matrix.append(row)
+        return matrix
+
+    def __str__(self) -> str:
+        matrix = self.generate_matrix()
+        return "Сгенерированная матрица:\n" + "\n".join(str(row) for row in matrix)
